c8ca03d8ade9bfc476e5fb520c73c873687cdc8f
m-akamine <m-akamine@it-college.ac.jp>
2025-07-04 08:38:41 +0000

    初期準備

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>WebTools</h1>
+        <h2>エレベーターシュミレーター</h2>
+      </main>
+      <footer className={styles.footer}></footer>
+    </div>
+  );
+}
