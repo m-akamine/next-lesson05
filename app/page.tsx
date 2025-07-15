cc357d5c55c89b9edc035cb220258b28f8c2ad69
m-akamine <m-akamine@it-college.ac.jp>
2025-07-15 16:38:47 +0900

    キューを追加

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,29 @@ import styles from "./page.module.css";
 
 export default function Home() {
   const [currentFloor, setCurrentFloor] = useState(1);
-  const [targetFloor, setTargetFloor] = useState(null);
-  useEffect(() => {
-    if(targetFloor !== null && targetFloor !== currentFloor){
-      const timer = setTimeout(()=> {
-        setCurrentFloor(prev => (prev < targetFloor ? prev + 1 : prev - 1));
-      }, 1000);
-      return () => clearTimeout(timer);
-      }
-    }, [currentFloor, targetFloor]);
+  const [queue, setQueue] = useState([]);
 
+  useEffect(() => {
+    if(queue.length > 0) {
+      const targetFloor = queue[0];
+
+      if(currentFloor !== targetFloor){
+        const timer = setTimeout(()=> {
+          setCurrentFloor(prev => (prev < targetFloor ? prev + 1 : prev - 1));
+        }, 1000);
+        return () => clearTimeout(timer);
+
+      }else {
+        setQueue(prevQueue => prevQueue.slice(1));
+      }
+
+    }
+  }, [currentFloor, queue]);
+  const addToQueue = (floor) => {
+    if (!queue.includes(floor)) {
+      setQueue(prevQueue => [...prevQueue, floor]);
+    }
+  }; 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
@@ -23,11 +36,12 @@ export default function Home() {
             <p>現在のフロア {currentFloor}</p>
             <div className={styles.buttons}>
               {[1, 2, 3, 4, 5, 6].map(floor => (
-                <button key={floor} onClick={() => setTargetFloor(floor)}>
+                <button key={floor} onClick={() => addToQueue(floor)}>
                   {floor}
                 </button>
               ))}
             </div>
+            <p>キュー {queue.join(', ')}</p>
           </div>
         </div>
       </main>
